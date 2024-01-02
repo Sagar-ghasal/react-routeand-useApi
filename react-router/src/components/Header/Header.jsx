fdcd84fbc528e624f756c0be8827ef9ad4496a84
Sagar-ghasal <153616757+Sagar-ghasal@users.noreply.github.com>
2024-01-02 05:53:53 +0000

    react-router-project

--- react-router/src/components/Header/Header.jsx
+++ react-router/src/components/Header/Header.jsx
@@ -32,10 +32,11 @@ export default function Header() {
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <NavLink
+                            <NavLink
                                 to="/"
-                                    className={(isActive) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-green-700" :"text-gray-700"}`
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 
+                                        ${isActive ? "text-orange-700" : "text-gray-700" } lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
@@ -44,13 +45,33 @@ export default function Header() {
                             <li>
                                 <NavLink
                                 to="/about"
-                                    className={(isActive) =>
+                                    className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" :"text-gray-700"}`
                                     }
                                 >
                                     About
                                 </NavLink>
-                            </li>                  
+                            </li> 
+                            <li>
+                                <NavLink
+                                to="/contact"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" :"text-gray-700"}`
+                                    }
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>                                       
+                            <li>
+                                <NavLink
+                                to="/github"
+                                    className={({isActive}) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" :"text-gray-700"}`
+                                    }
+                                >
+                                    GitHub
+                                </NavLink>
+                            </li>                     
                         </ul>
                     </div>
                 </div>
